850ed5576c56bc694347e8b6d491a71b786bbe5e
Unknown <harikrishnanirmal19@gnu.ac.in>
2020-08-07 22:13:08 +0530

    Weight & Activity Level Done in Screen 2

--- app/src/main/java/com/example/waterintake/Screen_2.java
+++ app/src/main/java/com/example/waterintake/Screen_2.java
@@ -15,10 +15,16 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.kevalpatel2106.rulerpicker.RulerValuePicker;
+import com.kevalpatel2106.rulerpicker.RulerValuePickerListener;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -28,15 +34,21 @@ import lib.kingja.switchbutton.SwitchMultiButton;
 public class Screen_2 extends AppCompatActivity {
 
   ConstraintLayout weight_button,activity_button,weather_button;
-  TextView user_weight,user_activity_level,weather;
+  TextView user_weight,user_activity_level,weather,Ruler_Title;
   EditText user_name;
   SwitchMultiButton gender;
+  RulerValuePicker RVP;
+  ListView activity_level_list;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_screen_2);
     controlbinding();
+    user_weight.setText("Weight in Kg");
+    final String[] activity_level_arrayList;
+
+//    final int w = Integer.parseInt(user_weight.toString());
 
     weight_button.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -49,7 +61,58 @@ public class Screen_2 extends AppCompatActivity {
         d.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         d.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         d.getWindow().setGravity(Gravity.BOTTOM);
+        RVP = d.findViewById(R.id.ruler_picker);
+        Ruler_Title = d.findViewById(R.id.Ruler_Title);
+        RVP.setValuePickerListener(new RulerValuePickerListener() {
+          @Override
+          public void onValueChange(final int selectedValue) {
+//            Toast.makeText(Screen_2.this,Integer.toString(selectedValue), Toast.LENGTH_SHORT).show();
+//            user_weight.setText(Integer.toString(selectedValue)+" Kg");
+          }
+
+          @Override
+          public void onIntermediateValueChange(final int selectedValue) {
+            user_weight.setText(Integer.toString(selectedValue)+" Kg");
+            Ruler_Title.setText("Your Weight "+ Integer.toString(selectedValue) +" Kg");
+
+          }
+        });
+
         d.show();
+
+      }
+    });
+
+    activity_level_arrayList = getResources().getStringArray(R.array.activity_level);
+    final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,activity_level_arrayList);
+
+
+    activity_button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+
+        final Dialog d = new Dialog(Screen_2.this);
+        d.setTitle("NumberPicker");
+        d.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        d.setContentView(R.layout.activity_level_dialog);
+        d.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        d.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        d.getWindow().setGravity(Gravity.BOTTOM);
+        activity_level_list = d.findViewById(R.id.activity_level_list);
+        activity_level_list.setAdapter(adapter);
+
+        activity_level_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+          @Override
+          public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            String activity = activity_level_arrayList[i];
+//            Toast.makeText(Screen_2.this, activity, Toast.LENGTH_SHORT).show();
+            user_activity_level.setText(activity);
+          }
+        });
+
+        d.show();
+
+
 
       }
     });
